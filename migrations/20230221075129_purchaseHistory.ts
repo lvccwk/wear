bfee856dee3f3e59ce7bc21b7ff8f90fcacb3959
Anthony <antonyeung919@gmail.com>
2023-02-21 15:53:01 +0800

    edit migration

--- migrations/20230221075129_purchaseHistory.ts
+++ migrations/20230221075129_purchaseHistory.ts
@@ -1,16 +1,16 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-	await knex.schema.createTable('purchaseHistory', (table) => {
+	await knex.schema.createTable('purchase_history', (table) => {
 		table.increments();
 		table.string('name').notNullable();
         table.integer("user_id").unsigned();
-        table.foreign("user_id").references("users_id");
+        table.foreign("user_id").references("users.id");
 
 		table.timestamps(false, true);
 	});
 }
 
 export async function down(knex: Knex): Promise<void> {
-	await knex.schema.dropTableIfExists('purchaseHistory');
+	await knex.schema.dropTableIfExists('purchase_history');
 }
