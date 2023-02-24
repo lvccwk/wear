6804ed24cff2192646e1eec6409e2f54bffd3183
freyy9 <0fr3ddy0@gmail.com>
2023-02-24 17:55:37 +0800

    f

---
+++ template_design/js/search-bar.js
@@ -0,0 +1,30 @@
+
+// const clearInput = () => {
+//     const input = document.getElementsByTagName("input")[0];
+//     input.value = "";
+//   }
+  
+// const clearBtn = document.getElementById("clear-btn");
+// clearBtn.addEventListener("click", clearInput);
+  
+  $(document).ready(function(){
+    $("#search").focus(function() {
+      $(".search-box").addClass("border-searching");
+      $(".search-icon").addClass("si-rotate");
+    });
+    $("#search").blur(function() {
+      $(".search-box").removeClass("border-searching");
+      $(".search-icon").removeClass("si-rotate");
+    });
+    $("#search").keyup(function() {
+        if($(this).val().length > 0) {
+          $(".go-icon").addClass("go-in");
+        }
+        else {
+          $(".go-icon").removeClass("go-in");
+        }
+    });
+    $(".go-icon").click(function(){
+      $(".search-form").submit();
+    });
+});
