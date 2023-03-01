2903acbc680bd98b76f8b3de5ccf4c4b30295f3b
Lawrence <lawrence_@live.hk>
2023-03-01 15:11:17 +0800

    end to end testcase

--- login.test.ts
+++ login.test.ts
@@ -12,32 +12,37 @@ describe('Login', () => {
 		page = await browser.newPage();
 	});
 
-	it('login', async () => {
+	// it('login', async () => {
+	// 	await page.goto('http://localhost:8080/');
+	// 	await page.getByRole('link', { name: '登入' }).click();
+	// 	await page
+	// 		.locator('#signin-form')
+	// 		.getByRole('button', { name: 'Sign In', exact: true })
+	// 		.click();
+	// });
+
+	it('test', async () => {
 		await page.goto('http://localhost:8080/');
 		await page.getByRole('link', { name: '登入' }).click();
 		await page
 			.locator('#signin-form')
 			.getByRole('button', { name: 'Sign In', exact: true })
 			.click();
-	});
+		await page.getByRole('link', { name: 'Go somewhere' }).first().click();
+		await page.getByRole('button', { name: 'CHECKOUT' }).click();
+		await page.click("[id='email']");
+		await page.locator('#email').fill('test@test.test');
+		await page.getByPlaceholder('1234 1234 1234 1234').click();
+		await page.getByPlaceholder('1234 1234 1234 1234').fill('4242 4242 4242 4242');
 
-	// it('register', async () => {
-	// 	await page.goto('http://localhost:8080/');
-	// 	await page.getByRole('link', { name: '登入' }).click();
-	// 	await page.locator('#signUp').click();
-	// 	await page.locator('#signup-form').getByPlaceholder('Email').click();
-	// 	await page.locator('#signup-form').getByPlaceholder('Email').fill('test_admin@com');
-	// 	await page.once('dialog', async (dialog) => {
-	// 		// await console.log(`Dialog message: ${dialog.message()}`);
-	// 		await dialog.dismiss().catch(() => {});
-	// 	});
-	// 	await page.locator('#signup-form').getByRole('button', { name: 'Sign Up' }).click();
-	// });
+		await page.click("[id='cardExpiry']");
+		await page.locator('#cardExpiry').fill('03 / 30');
 
-	// afterEach(async () => {
-	// 	// await knex.raw('delete from users where email = ?', ['test_admin@com']);
-	// 	await knex('users').where({
-	// 		email: 'test_admin@com'
-	// 	});
-	// });
+		await page.click("[id='cardCvc']");
+		await page.locator('#cardCvc').fill('333');
+
+		await page.click("[id='billingName']");
+		await page.locator('#billingName').fill('qqq');
+		await page.getByTestId('hosted-payment-submit-button').click();
+	}, 100000);
 });
