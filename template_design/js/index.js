d4025bae12eac7d68f4e69a5c3a70e2d94788c85
Lawrence <lawrence_@live.hk>
2023-03-03 15:37:43 +0800

    cd

--- template_design/js/index.js
+++ template_design/js/index.js
@@ -1,27 +1,25 @@
 //const searchbar = document.querySelector('.searchbar');
 const test = document.querySelector('.search-icon');
 test.addEventListener('click', async (e) => {
-   console.log('test');
+	console.log('test');
 });
 
-
-
 const input = document.querySelector('.search_input');
-let promptFormElem =  document.querySelector('.search-form');
+let promptFormElem = document.querySelector('.search-form');
 promptFormElem.addEventListener('submit', async (e) => {
-    e.preventDefault()
-    window.location = '/searchresult.html';
-    console.log('LOADING');
-    let uploadData = {prompt: promptFormElem.prompt.value}
-    console.log(uploadData.prompt);
+	e.preventDefault();
+	window.location = '/searchresult.html';
+	console.log('LOADING');
+	let uploadData = { prompt: promptFormElem.prompt.value };
+	console.log(uploadData.prompt);
 
-    const newMessage = document.createElement('p');
-    newMessage.textContent = uploadData;
-    //searchbar.appendChild(newMessage);
-    input.value = '';
-	console.log("indexjs_line15");
-    //let result = await fetch(`http://localhost:8000/search-result?prompt=${uploadData.prompt}`)
-    let response = await fetch('/prompt',{
+	const newMessage = document.createElement('p');
+	newMessage.textContent = uploadData;
+	//searchbar.appendChild(newMessage);
+	input.value = '';
+	console.log('indexjs_line15');
+	//let result = await fetch(`http://localhost:8000/search-result?prompt=${uploadData.prompt}`)
+	let response = await fetch('/prompt', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json; charset=utf-8'
@@ -29,22 +27,21 @@ promptFormElem.addEventListener('submit', async (e) => {
 		body: JSON.stringify(uploadData)
 	});
 
-    if (!response.ok) {
-        console.log("ERR0R");
-        return
-    }
-	console.log("indexjs_line29");
+	if (!response.ok) {
+		console.log('ERR0R');
+		return;
+	}
+	console.log('indexjs_line29');
 
-    const suggestions = await response.json();
-    console.log(suggestions)
-    //const responseMsg = document.createElement('p');
-    //responseMsg.textContent = suggestions.suggestions.suggestions;
-    //searchbar.appendChild(responseMsg);
-        if (suggestions.generating === 1) {
-            input.disabled = true;
-        }
-
-}); 
+	const suggestions = await response.json();
+	console.log(suggestions);
+	//const responseMsg = document.createElement('p');
+	//responseMsg.textContent = suggestions.suggestions.suggestions;
+	//searchbar.appendChild(responseMsg);
+	if (suggestions.generating === 1) {
+		input.disabled = true;
+	}
+});
 
 // const clearInput = () => {
 // 	const input = document.getElementsByTagName('input')[0];
@@ -76,3 +73,8 @@ promptFormElem.addEventListener('submit', async (e) => {
 // });
 
 // socket.emit('search_product', input.value, user_id);
+
+// post handling
+
+// let data = await res.json()
+// console.log(data);
